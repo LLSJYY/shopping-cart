1154512c14487993a191fae4cca127db64571be0
이쥬곰 <leesu@ijyugom-ui-MacBookAir.local>
2022-07-19 15:22:30 +0900

    list스크립트 수정으로 cart도 수정

--- cartScript.js
+++ cartScript.js
@@ -6,18 +6,14 @@ const numberFormat = function (str) {
  return str.toString()
     .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
 };
-
-(()=>{
-  
-})()
 /** LIST CART IMG CLICK EVENT */
 const newCartItem = function (e) {
   const section = document.querySelector(".cart-left-section");
   section.innerHTML += `<div class="cart-container">
   <div class="flex gap-15 mt-10">
     <input class="checkbox" name="checkbox" type="checkbox" checked="true">
-    <img class="w-144 h-144" src="./assets/images/product.png" alt="${e.getImgName}">
-    <span class="cart-name">${(e.getImgName)}</span>
+    <img class="w-144 h-144" src="./assets/images/product.png" alt="${e.productImg}">
+    <span class="cart-name">${(e.productName)}</span>
   </div>
   <div class="flex-col-center justify-end gap-15">
     <img class="cart-trash-svg" src="./assets/svgs/trash.svg" alt="삭제">
@@ -28,12 +24,11 @@ const newCartItem = function (e) {
         <button class="number-input-button">▼</button>
       </div>
     </div>
-    <span class="cart-price" data-price="${justNumbers(e.getImgPrice)}" >${e.getImgPrice}</span>
+    <span class="cart-price" data-price="${justNumbers(e.productPrice)}" >${e.productPrice}</span>
   </div>
   </div>
   <hr class="divide-line-thin mt-10" />`
 }
-
 const getStorage = function () {
   const cartSection = document.querySelector(".cart-left-section");
   const getLocalStorage = JSON.parse(localStorage.getItem('newCartItem'));
