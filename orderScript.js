123a1379b6637d04e57cbd90f18c0b328b59edf1
hyunjun-kim7 <hyunjun.kim7@navercorp.com>
2022-07-24 16:48:21 +0900

    refactor: 주문 결제 페이지 리팩토링
    주문결제 페이지 포맷팅 기능 추가

--- orderScript.js
+++ orderScript.js
@@ -1,34 +1,35 @@
+const storeKey = "cart-item-list";
+const productList = JSON.parse(localStorage.getItem(storeKey)) || [];
+const numberFormat = function (str) {
+  return str.toString()
+    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+};
 const newCartItem = function (e) {
-    const section = document.querySelector(".order-left-section");
-    section.innerHTML += 
-    `<div class="order-container">
-    <div class="flex gap-15 mt-10">
-      <img
-        class="w-144 h-144"
-        src="${e.productImg}"
-        alt="${e.productName}"
-      />
-      <div class="flex-col gap-15">
-        <span class="order-name">${e.productName}</span>
-        <span>수량: ${e.productQty}</span>
-      </div>
-    </div>
-    </div>
-    <hr class="divide-line-thin mt-10" />`
-  }
-  const getStorage = function () {
-    const getLocalStorage = JSON.parse(localStorage.getItem('newCartItem'));
-    getLocalStorage.forEach(el => {
-   console.log(el)
-        newCartItem(el)
-    });
-  }
+  const section = document.querySelector(".order-left-section");
+  section.innerHTML +=
+  /*html*/`<div class="order-container">
+            <div class="flex gap-15 mt-10">
+              <img class="w-144 h-144" src="${e.productImg}" alt="${e.productName}" />
+              <div class="flex-col gap-15">
+                <span class="order-name">${e.productName}</span>
+                <span>수량: ${e.count}</span>
+              </div>
+            </div>
+          </div>
+          <hr class="divide-line-thin mt-10" />`
+}
 
-  getStorage()
+productList.forEach(newCartItem);
 
-  (function(){
-    const section = document.querySelector(".order-left-section");
-    const itemsPrice =  document.querySelectorAll(".order-container").forEach();  
-    const orderTotal =  section.querySelectorAll(".highlight-text")[1].innerText
-    
-  })()
+const updateTotalPrice = function () {
+  const totalPrice = productList.filter(({ checked }) => checked).reduce((acc, curr) => {
+    const { productPrice, count } = curr;
+
+    return acc + parseInt(productPrice.replace(/[^0-9]/g, "")) * count;
+  }, 0);
+
+  document.querySelectorAll(".highlight-text")[1].innerHTML = `${numberFormat(totalPrice)}원`;
+  document.querySelector(".primary-button.flex-center").innerHTML = `${numberFormat(totalPrice)}원 결재하기`;
+}
+
+updateTotalPrice();
