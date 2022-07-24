ad6554a0204647020c721f62c491663fe62d894d
hyunjun-kim7 <hyunjun.kim7@navercorp.com>
2022-07-24 14:11:21 +0900

    refactor: 상품 목록 페이지 리팩토링 작업
    상품 목록 페이지 리팩토링

--- listScript.js
+++ listScript.js
@@ -1,47 +1,34 @@
-const storedProduct = []; // 이름이 맘에 안듭니다.
+const productList = JSON.parse(localStorage.getItem("cart-item-list")) || [];
 const productContainer = document.querySelector(".product-container");
-(function () {
-   if(JSON.parse(localStorage.getItem('newCartItem'))!=null){
-    JSON.parse(localStorage.getItem('newCartItem')).forEach(e => {
-    storedProduct.push(e)})
-  }})();
 
 const storeLocalStorage = function () {
-  localStorage.setItem('newCartItem', JSON.stringify(storedProduct));
-}
+  localStorage.setItem("cart-item-list", JSON.stringify(productList));
+};
 
-let count = 0;
+productContainer.addEventListener("click", (e) => {
+  const itemContainer = e.target.closest(".item-container");
+  const productId = itemContainer.getAttribute("data-id");
+  const productImg = itemContainer.querySelector("img").getAttribute("src"); 
+  const productName = itemContainer.querySelector(".product-info__name").innerText;
+  const productPrice = itemContainer.querySelector(".product-info__price").innerText;
 
-productContainer.addEventListener('click', (e) => {
   if (e.target.getAttribute("alt") == "장바구니") {
-    const productInfo = e.target.closest("div")
-    const imgSrcCart = productInfo.querySelector("img").getAttribute("src");
-    const productImg = e.target.closest("div").parentElement.querySelector("img").getAttribute("src");
-    const productName = productInfo.querySelector(".product-info__name").innerText;
-    const productPrice = productInfo.querySelector(".product-info__price").innerText;
-    console.log(productImg, productName, productPrice)
+    const findProductItem = productList.filter(({ productId: _productId }) => {
+      return _productId === productId;
+    })[0];
 
-    count++;
-    storedProduct.push({
+    const count = findProductItem ? findProductItem.count : 0;
+
+    productList.push({
+      productId,
       productImg,
       productName,
       productPrice,
-      count,
-    })
-   
-    storeLocalStorage();
-    // location.href = `./cart.html?img=${productImg}&name=${productName}&Price=${productPrice}`//todo : yes or no로 넘어갈지 안넘어갈지 하기
-    console.log(storedProduct) // todo : yes or no로 넘어갈지 안넘어갈지 하기
-  }
-  if (e.target.tagName === "IMG" && e.target.getAttribute("alt") != "장바구니") {
-    const productInfo = e.target.closest("div")
-    const imgSrcCart = productInfo.querySelector("img").getAttribute("src");
-    const productImg = e.target.closest("div").parentElement.querySelector("img").getAttribute("src");
-    const productName = productInfo.querySelector(".product-info__name").innerText;
-    const productPrice = productInfo.querySelector(".product-info__price").innerText;
-    console.log(productImg, productName, productPrice)
-    location.href = `./detail.html?img=${productImg}&name=${productName}&Price=${productPrice}`
+      count: count + 1,
+    });
 
-    console.log(e)
+    storeLocalStorage();
+  } else {
+    location.href = `./detail.html?img=${productImg}&name=${productName}&price=${productPrice}`
   }
-})
+});
