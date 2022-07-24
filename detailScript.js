e01426dc5043882af6545f7f5497af5dc9996006
hyunjun-kim7 <hyunjun.kim7@navercorp.com>
2022-07-24 14:39:12 +0900

    refactor: 상품 상세 페이지 리팩토링 작업
    queryString 변수 분리
    html 보기 좋게 포맷팅
    productData reduce를 이용한 object로 변경
    queryString이 없는 경우 예외 처리

--- detailScript.js
+++ detailScript.js
@@ -1,32 +1,36 @@
 const getProduct = function () {
-  const getUrl = location.href.split("?")[1].split("&").map((item) => {
-    const [key, value] = item.split("=")
-    return { [key]: decodeURI(value) }
-  })
-  console.log(getUrl);
+  const queryString = location.href.split("?")[1];
 
-  ///그냥 상품상세 띄웠을때 아무것도 안나오는데 ㅇ어떤방법이 좋을까요 
-document.querySelector(".product-detail-container").innerHTML +=
-  `<div class="flex-col-center w-520">
-<img class="w-480 h-480 mb-10"
-  src="${getUrl[0].img}"
-  alt="${getUrl[1].name}"
-/>
-<div class="product-detail-info">
-  <span class="product-detail-info__name">${getUrl[1].name}</span>
-  <hr class="divide-line-gray my-20" />
-  <div class="flex justify-between">
-    <span>금액</span>
-    <span class="product-detail-info__price">${getUrl[2].Price}</span>
-  </div>
-</div>
-<button class="product-detail-button flex-center mt-20">
-  장바구니
-</button>`
+  if (queryString) {
+    const params = queryString.split("&").map((item) => {
+      const [key, value] = item.split("=");
 
-}
+      return {
+        [key]: decodeURI(value)
+      };
+    });
+    
+    const productData = params.reduce((acc, curr) => {
+      // map, filter, reduce 순수 함수 똑같은 값이 들어오면 똑같은 output을 만들어내고 side effect가 생기지 않는 함수
+      return Object.assign({}, acc, curr); // 원본에 영향을 미치지 않는다.
+    }, {}); // 어떤 타입으로 변경해주는지 이해하기 쉽게 두번째 값을 넣어준다.
+
+    document.querySelector(".product-detail-container").innerHTML +=
+      /*html*/`<div class="flex-col-center w-520">
+                <img class="w-480 h-480 mb-10" src="${productData.img}" alt="${productData.name}" />
+                <div class="product-detail-info">
+                  <span class="product-detail-info__name">${productData.name}</span>
+                  <hr class="divide-line-gray my-20" />
+                  <div class="flex justify-between">
+                    <span>금액</span>
+                    <span class="product-detail-info__price">${productData.price}</span>
+                  </div>
+                </div>
+                <button class="product-detail-button flex-center mt-20">
+                  장바구니
+                </button>
+              </div>`;
+  }
+};
+
 getProduct();
-
-
-
-
